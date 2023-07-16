478f0bb0c577bdad4fa25bc8212d2719e746d549
freelance <freelance@gamil.com>
2023-07-16 19:01:21 +0800

    update web

--- common.js
+++ common.js
@@ -6,19 +6,28 @@
     
     recalc = function() {
     
-    var clientWidth = docEl.clientWidth;
+    // var clientWidth = docEl.clientWidth;
+       
+    var clientWidth = window.innerWidth;
     
     if (!clientWidth) return;
     
     // console.log(clientWidth)
-    if (clientWidth >= 750) {
-    
+    if (clientWidth > 1023) {
     docEl.style.fontSize = '16px';
-    // docEl.style.fontSize = 100 * (clientWidth / 1920) + 'px';
-    
+    // if(clientWidth<=1240){
+    //     docEl.style.fontSize = 100 * (1240 / 1920) + 'px';
+    // }else{
+    //     docEl.style.fontSize = 100 * (clientWidth / 1920) + 'px';
+    // }
+    // docEl.style.fontSize=16*(clientWidth / 1920) + 'px'
+  
     } else {
+      
     docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
-    
+    if(clientWidth > 750){
+        docEl.style.fontSize='100px'
+     }
     }
     
     };
